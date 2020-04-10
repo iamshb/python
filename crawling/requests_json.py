76d6d6aa4d4972a3793b54f738bce5f29adf95f9
shinshin <iamshb@linecorp.com>
2020-04-11 01:30:06 +0900

    add parsing json with requests lib sample

---
+++ crawling/requests_json.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+s = requests.Session()
+r = s.get('https://jsonplaceholder.typicode.com/albums')
+print(r.json())
+
+s.close()
+
+
+with requests.Session() as s:
+    r = s.get('http://httpbin.org/stream/20')
+
+    for line in r.iter_lines(decode_unicode='utf-8'):
+        # print(type(line)) -> binary
+        json_str = json.loads(line)
+        # print(type(json_str)) -> dict
+        for e in json_str.keys():
+            print('#', e, ':', json_str[e])
